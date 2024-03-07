37621c6a5092819d0f25fc3b6a4e5482e31d4a1a
Anuj Kulkarni <anujkulkarni16@gmail.com>
2024-03-07 10:50:57 +0530

    Header Page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,18 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './Pages/Home'
 import Signin from './Pages/Signin'
 import Signup from './Pages/Signup'
+import Profile from './Pages/Profile'
+import Header from './component/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/signin' element={<Signin/>}/>
         <Route path='/signup' element={<Signup/>}/>
+        <Route path='/profile' element={<Profile/>}/>
       </Routes>
     </BrowserRouter>
   )
