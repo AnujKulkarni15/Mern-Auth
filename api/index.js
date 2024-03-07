5b85f6bb396b4f983e7fc4a2eb035c586629fdf5
Anuj Kulkarni <anujkulkarni16@gmail.com>
2024-03-07 11:27:05 +0530

    Database Connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+
+mongoose.connect('mongodb+srv://anujkulkarni16:anuj@mern.vtj0auj.mongodb.net/mern-auth?retryWrites=true&w=majority&appName=mern').then(()=>{
+  console.log('connected to MongoDB')
+}).catch((err) => {
+  console.log(err)
+})
 
 const app = express()
 
